5871e274e11ed616dba09135d0d38c36875e9f62
Kapil Chaudhary <kc685838@gmail.com>
2023-05-02 17:48:11 +0530

    submitting code-Tue May 02 2023 17:48:09 GMT+0530 (India Standard Time)

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,15 @@
+import React from 'react';
 
+const Selection = (props) => {   
+    function helper(e){
+        console.log(e.target);
+        e.target.style.backgroundColor = applyColor.background;
+    }
+   
+    const { applyColor } = props;
+    console.log("Selection applyColor background is == ", applyColor.background);
+return <div className='fix-box' onClick={(e) => {helper(e)}}>        
+    </div>;
+}
+
+export default Selection;
